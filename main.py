01e1659a71839bab3795f71a00022d0006e64eee
Kuroyukihime000 <mishutka2282@tutamail.com>
2025-04-25 23:22:22 +0300

    completed

---
+++ main.py
@@ -0,0 +1,9 @@
+from qweq import OperationResult
+
+def main():
+    first=OperationResult()
+    +first
+    first.partition()
+    
+if __name__ == '__main__':
+    main()
---
+++ qweq.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class OperationResult:
+    def __init__(self):
+        self.data = pd.read_csv('var8.csv')
+        self.df = self.data.copy() 
+
+    def __pos__(self):
+        value1 = self.df.shape[0] 
+        self.df = self.df.drop_duplicates()
+        value2 = self.df.shape[0] 
+        deletion = value1 - value2
+        print(f'В файле удалено дубликатов: {deletion}')
+
+    def partition(self):
+        dataframe1 = self.df[self.df['Результат операции'] == 'недостаточно средств']
+        dataframe2 = self.df[self.df['Результат операции'] == 'успешно']   
+        dataframe3 = self.df[self.df['Результат операции'] == 'неправильный ввод пинкода']   
+        
+        dataframe1.to_csv('setfirst.csv', index = False)
+        dataframe2.to_csv('setsecond.csv', index = False)
+        dataframe3.to_csv('setthird.csv', index = False)
+
+    def __del__(self):
+        print('Удалено')
